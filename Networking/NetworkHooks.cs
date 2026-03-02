0355fcad713ade7112d08c17d1d62fce45cf2fd9
D-Bunch <dombunchcollinsa@gmail.com>
2026-03-02 17:04:53 -0600

    Create NetworkHooks.cs

---
+++ Networking/NetworkHooks.cs
@@ -0,0 +1,13 @@
+// NOTE: This file is a placeholder. Add Mirror (or your networking package) and change classes
+// to derive from NetworkBehaviour and implement syncs. Example: Sync player vitals to server.
+
+using UnityEngine;
+
+public class NetworkHooks : MonoBehaviour
+{
+    // Example: call this to broadcast hr to server later
+    public void SendHeartRateToServer(float hr)
+    {
+        // TODO: implement Mirror Command or UNet RPC
+    }
+}
